42b4c445c02436b63b0af1cf0bd539f23fd4d095
ThatFrikinNoob <50806284+ThatFrikinNoob@users.noreply.github.com>
2019-07-09 23:05:26 -0400

    attempt a header for pine

---
+++ pine.h
@@ -0,0 +1,14 @@
+#ifndef PINE
+#define PINE
+
+ DevicePin *getPinAddress(int id);
+ int digitalReadPin(DigitalPin name);
+ void digitalWritePin(DigitalPin name, int value);
+ int analogReadPin(AnalogPin name);
+ void analogWritePin(AnalogPin name, int value);
+ void analogSetPeriod(AnalogPin name, int micros);
+ void onPulsed(DigitalPin name, PulseValue pulse, Action body);
+ int pulseDuration();
+ int pulseIn(DigitalPin name, PulseValue value, int maxDuration = 2000000);
+ 
+ #endif
